d32e7e1fa2080888cfd3a252f11c13e55ddfbb8d
Yogesh Bhatt <yogesh.bhatt@appdirect.com>
2024-02-21 16:20:30 +0530

    feat: add-feedback api route

---
+++ src/app/api/add-feedback/route.ts
@@ -0,0 +1,55 @@
+/* eslint-disable no-useless-catch */
+import { GoogleAuth } from 'google-auth-library';
+import { google } from 'googleapis';
+import { NextApiRequest } from 'next';
+import { NextResponse } from 'next/server';
+
+const auth = new GoogleAuth({
+  scopes: 'https://www.googleapis.com/auth/spreadsheets',
+  credentials: {
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+  },
+});
+
+const sheets = google.sheets({ version: 'v4', auth });
+
+async function addRecordsToSheet(records: Array<Array<string>>) {
+  const spreadsheetId = '1lnZXY56yx6EH-um4EpnZILxV1ld0W1QM8bMVHbWmn0M';
+  const range = 'feedback-sheet';
+  try {
+    const response = await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range,
+      valueInputOption: 'USER_ENTERED',
+      requestBody: {
+        values: records,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function POST(req: NextApiRequest) {
+  const body = await req.json();
+
+  const {
+    prompt,
+    answer: { ProductLine, Category, Subcategory },
+  } = body;
+
+  const records = [[prompt, ProductLine, Category, Subcategory]];
+
+  try {
+    await addRecordsToSheet(records);
+  } catch (error) {
+    return NextResponse.json({ msg: 'An Error occured!' });
+  }
+
+  return NextResponse.json({ msg: 'Data added successfully!' });
+}
